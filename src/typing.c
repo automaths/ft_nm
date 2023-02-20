774df80fe312815e262303acd0f9b3dd5f5efb65
automaths <nicolassartral@gmail.com>
2023-02-20 12:58:56 +0100

    correcting symbol types

--- src/typing.c
+++ src/typing.c
@@ -1,9 +1,55 @@
 #include "ft_nm.h"
 
+//i section specific for implementation of DLLs
+//I indirect reference to another symbol
+//n in read-only data section
+//S s unitializaed section for small objects
+//W w weak symbol not tagged as weak symbol
+
+// char	symtab_section_type(Elf64_Sym sym, Elf64_Shdr *shdr)
+// {
+//     char c;
+
+//     if (sym.st_shndx == SHN_ABS)
+//         return 'A';
+//     else if (shdr[sym.st_shndx].sh_type == SHT_NOBITS)
+//     {
+//         if (sym.st_shndx == 26)
+//             c = 'B';
+//         else
+//             c = 'D';
+//         //G in unitialized for small objects
+//     }
+//     else if (sym.st_shndx == SHN_COMMON)
+//         c = 'C';
+//     else if (shdr[sym.st_shndx].sh_type == SHT_ALPHA_DEBUG)
+//         return 'N';
+//     else if (shdr[sym.st_shndx].sh_type == SHT_PARISC_UNWIND)
+//         return 'p';
+//     else if (shdr[sym.st_shndx].sh_type == SHT_PROGBITS
+//              && shdr[sym.st_shndx].sh_flags == SHF_ALLOC)
+//         c = 'R';
+//     else if (shdr[sym.st_shndx].sh_type == SHT_PROGBITS
+//              && shdr[sym.st_shndx].sh_flags == (SHF_ALLOC | SHF_EXECINSTR))
+//         c = 'T';
+//     else if (sym.st_info == 32)
+//         c = 'W';
+//     else if (ELF64_ST_BIND(sym.st_info) == STB_GNU_UNIQUE)
+//         return 'u';
+//     else if (sym.st_shndx == SHN_UNDEF)
+//         return 'U';    
+//     else if (ELF64_ST_BIND(sym.st_info) == STB_WEAK)
+//         c = 'V';
+//     else
+//         return '?';
+//     if (ELF64_ST_BIND(sym.st_info) == STB_LOCAL && c != '?')
+//         c += 32;
+//     return c;
+// }
+
 char	symtab_section_type(Elf64_Sym sym, Elf64_Shdr *shdr)
 {
     char    c;
-
     if (ELF64_ST_BIND(sym.st_info) == STB_GNU_UNIQUE)
         c = 'u';
     else if (sym.st_shndx == SHN_UNDEF)
@@ -50,7 +96,9 @@ char	symtab_section_type(Elf64_Sym sym, Elf64_Shdr *shdr)
 // B 1 0 16 26 0, 1 0 16 26 0, 
 // b 0 1 1 26 0, 
 // C
+
 // D 1 0 16 25 0, 1 0 16 25 0, 1 1 17 25 2, 1 1 17 25 2,
+
 // d 0 1 1 22 0, 0 1 1 21 0, 0 1 1 24 0, 0 1 1 23 0,
 // G
 // g
