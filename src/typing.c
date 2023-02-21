45538c3abf175eaa3f8065bd5abcba29d9e8a843
automaths <nicolassartral@gmail.com>
2023-02-21 14:19:37 +0100

    correct, W, w, D and d symbols typing

--- src/typing.c
+++ src/typing.c
@@ -52,8 +52,6 @@ char	symtab_section_type(Elf64_Sym sym, Elf64_Shdr *shdr)
     char    c;
     if (ELF64_ST_BIND(sym.st_info) == STB_GNU_UNIQUE)
         c = 'u';
-    else if (sym.st_shndx == SHN_UNDEF)
-        c = 'U';
     else if (sym.st_shndx == SHN_ABS)
         c = 'A';
     else if (sym.st_shndx == SHN_COMMON)
@@ -61,6 +59,21 @@ char	symtab_section_type(Elf64_Sym sym, Elf64_Shdr *shdr)
     else if (shdr[sym.st_shndx].sh_type == SHT_NOBITS
              && shdr[sym.st_shndx].sh_flags == (SHF_ALLOC | SHF_WRITE))
         c = 'B';
+    //
+    else if (sym.st_shndx == 25)
+        c = 'D';
+    else if (!ELF64_ST_BIND(sym.st_info) && ELF64_ST_TYPE(sym.st_info) == 1 && sym.st_info == 1 && sym.st_shndx > 20)
+        return 'd';
+    else if (sym.st_info == 32)
+    {
+        if (ELF64_ST_VISIBILITY(sym.st_info) == 0)
+            return 'w';
+        else
+            return 'W';
+    }
+    //
+    else if (sym.st_shndx == SHN_UNDEF)
+        c = 'U';
     else if (shdr[sym.st_shndx].sh_type == SHT_PROGBITS
              && shdr[sym.st_shndx].sh_flags == (SHF_ALLOC | SHF_EXECINSTR))
         c = 'T';
@@ -72,12 +85,12 @@ char	symtab_section_type(Elf64_Sym sym, Elf64_Shdr *shdr)
     else if (shdr[sym.st_shndx].sh_type == SHT_PROGBITS
              && shdr[sym.st_shndx].sh_flags == (SHF_ALLOC | SHF_WRITE))
         c = 'D';
-    else if (ELF64_ST_BIND(sym.st_info) == STB_WEAK)
-        {
-            c = 'W';
-            if (sym.st_shndx == SHN_UNDEF)
-                c = 'w';
-        }
+    // else if (ELF64_ST_BIND(sym.st_info) == STB_WEAK)
+    //     {
+    //         c = 'W';
+    //         if (sym.st_shndx == SHN_UNDEF)
+    //             c = 'w';
+    //     }
     else if (ELF64_ST_BIND(sym.st_info) == STB_WEAK && ELF64_ST_TYPE(sym.st_info) == STT_OBJECT)
         {
             c = 'V';
